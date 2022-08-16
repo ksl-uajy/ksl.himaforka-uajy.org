68276e111784d4f1e0eadee5af3e008fa91806ff
ethan <benidiktusvio@gmail.com>
2022-08-16 22:22:25 +0700

    test dulu

--- src/components/molecule/Schedule.jsx
+++ src/components/molecule/Schedule.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Cardinstall from "../atom/Cardinstall";
-import { dbMateri } from "../../../data/DataMateri";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const Schedule = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            let fetchedData = await fetch("https://ksl-himaforka.web.app/api/v1/jadwal-ksl");
+            fetchedData = await fetchedData.json();
+            fetchedData.shift();
+            setData(fetchedData);
+        }
+        fetchData();
+    }, []);
+
 
     const slideLeft = () => {
         var slider = document.getElementById('slider');
         slider.scrollLeft = slider.scrollLeft - 500;
-    }
+    };
 
     const slideRight = () => {
         var slider = document.getElementById('slider');
         slider.scrollLeft = slider.scrollLeft + 500;
-    }
+    };
 
     return (
         <div className="md:w-[44rem] lg:w-[56rem] xl:w-[72rem] mx-auto ">
@@ -22,40 +33,53 @@ const Schedule = () => {
             <div className="flex relative items-center mt-3 md:mt-4">
                 <MdChevronLeft className="hidden lg:block opacity-80 cursor-pointer hover:opacity-75 hover:scale-150 mr-2" size={30} onClick={slideLeft} />
                 <div id="slider" className=" xl:w-[56rem] lg:w-[42rem] md:w-[36rem] h-full flex snap-x overflow-x-auto whitespace-nowrap scrollbar-hide scroll-smooth rounded-2xl font-light">
-                    {dbMateri.map(data => {
+                    {/* {dbMateri.map(data => {
                         if (data.id == 99) {
                             return (<div key={data.id} className="bg-[#227154] rounded-2xl py-4 px-4 mx-2 cursor-pointer">
                                 <h1 className="text-[16px]">{data.tgl}</h1>
                                 <h1 className="text-[14px]">{data.Materi}</h1>
-                            </div>)
+                            </div>);
                         } else if (data.id == 88) {
                             return (
                                 <div key={data.id} className="bg-[#791E3F] rounded-2xl py-4 px-4 mx-2 cursor-pointer">
                                     <h1 className="text-[16px]" >{data.tgl}</h1>
                                     <h1 className="text-[14px]">{data.Materi}</h1>
                                 </div>
-                            )
+                            );
                         } else if (data.id % 2 == 0) {
                             return (
                                 <div key={data.id} className="bg-trmColor rounded-2xl py-4 px-4 mx-2 cursor-pointer">
                                     <h1 className="text-[16px]">{data.tgl}</h1>
                                     <h1 className="text-[14px]">{data.Materi}</h1>
                                 </div>
-                            )
+                            );
                         } else if (data.id % 2 == 1) {
                             return (
                                 <div key={data.id} className="bg-cardColor rounded-2xl py-4 px-4 mx-2 cursor-pointer">
                                     <h1 className="text-[16px]" >{data.tgl}</h1>
                                     <h1 className="text-[14px]">{data.Materi}</h1>
                                 </div>
-                            )
+                            );
                         }
-                    })}
+                    })} */}
+
+                    {data && data.length > 0 &&
+                        <>
+                            {data.map(data => (
+
+                                <div key={data[0]} className="bg-[#791E3F] rounded-2xl py-4 px-4 mx-2 cursor-pointer">
+                                    <h1 className="text-[16px]" >{data[1]}</h1>
+                                    <h1 className="text-[14px]">{data[2]}</h1>
+                                </div>
+                            ))}
+                        </>
+                    }
+
                 </div>
                 <MdChevronRight className="hidden lg:block opacity-80 cursor-pointer hover:opacity-75 hover:scale-150 ml-2" size={30} onClick={slideRight} />
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Schedule;
