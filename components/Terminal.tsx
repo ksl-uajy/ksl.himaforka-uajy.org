470f435551ebe021a6bee835d607cc932e14c314
Alexis Ngaing <ksankirara92@gmail.com>
2023-08-02 12:06:13 +0700

    fix Terminal

--- components/Terminal.tsx
+++ components/Terminal.tsx
@@ -8,7 +8,7 @@ const Terminal: FC = () => {
   return (
     <>
       <div className="mx-auto flex items-center justify-center py-10">
-        <div className="h-[30rem] w-11/12 rounded-2xl bg-[#102C27] leading-[18.5px] md:h-[rem] md:w-8/12">
+        <div className="h-96 w-11/12 rounded-2xl bg-[#102C27] leading-[18.5px] md:h-[30rem] md:w-8/12">
           <div className="h-10 w-full rounded-t-2xl bg-[#20443E]">
             <h1 className="px-6 py-3 font-rubik text-[#FFEDBF]">😺 NekoTerm</h1>
           </div>
@@ -18,7 +18,7 @@ const Terminal: FC = () => {
                 <span className="text-[#D4FFBA]">{"> "}ksl</span> art
               </p>
               <br />
-              <p className="font-mono text-[8px] text-[#C3FFF4] md:text-lg">
+              {/* <p className="font-mono text-[8px] text-[#C3FFF4] md:text-lg">
                 &nbsp;/-------&emsp;&nbsp;&emsp;&emsp;▓&nbsp;&nbsp;▓&nbsp;&ensp;▓▓▓▓&nbsp;&ensp;▓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&ensp;&nbsp;&nbsp;&nbsp;▓&nbsp;&nbsp;▓&nbsp;&nbsp;▓▓▓▓&emsp;&emsp;&emsp;&emsp;&emsp;▓&nbsp;&nbsp;▓&nbsp;&nbsp;▓{" "}
                 <br />/ &emsp;&nbsp; 0 o \ &ensp;
                 ▓&nbsp;▓&nbsp;&ensp;&nbsp;▓&nbsp;&nbsp;&ensp;&nbsp;&nbsp;▓&nbsp;&ensp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;▓&nbsp;&nbsp;▓&nbsp;&nbsp;▓&nbsp;&nbsp;▓&emsp;&emsp;&emsp;&emsp;&emsp;▓&nbsp;&nbsp;▓&nbsp;&nbsp;▓{" "}
@@ -29,14 +29,21 @@ const Terminal: FC = () => {
                 ▓&nbsp;▓&nbsp;&nbsp;&ensp;&nbsp;&nbsp;&nbsp;▓&nbsp;&ensp;▓&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&ensp;&nbsp;&nbsp;&nbsp;&nbsp;&ensp;▓&nbsp;&nbsp;▓&nbsp;&nbsp;▓&nbsp;&nbsp;▓&emsp;&emsp;&emsp;&emsp;&emsp;▓&nbsp;&nbsp;&nbsp;&nbsp;▓{" "}
                 <br />
                 ----------&ensp;&nbsp;&nbsp;▓&nbsp;&ensp;▓&nbsp;&ensp;▓▓▓▓&nbsp;&ensp;▓▓▓▓&nbsp;&nbsp;&nbsp;&ensp;&nbsp;&nbsp;&nbsp;&ensp;▓▓▓▓&nbsp;&nbsp;▓&nbsp;&nbsp;▓&nbsp;&nbsp;▓▓▓▓&ensp;&ensp;&nbsp;▓▓
-              </p>
+              </p> */}
 
-              {/* <p className="font-mono text-lg text-[#C3FFF4]">
-                /-------\   ▓  ▓  ▓▓▓▓  ▓    <br></br>
-                |    0 o \  ▓ ▓   ▓     ▓    <br></br>
-                |       &gt;|  ▓▓    ▓▓▓▓  ▓ <br></br>
-                |~~~~~~~~|  ▓ ▓      ▓  ▓    <br></br>
-                ----------  ▓  ▓  ▓▓▓▓  ▓▓▓▓ <br></br>
+              <p className="font-mono text-[7px] text-[#C3FFF4] md:text-lg">
+                /-------\   ▓  ▓  ▓▓▓▓  ▓          ▓  ▓  ▓▓▓▓     ▓  ▓  ▓<br></br>
+                |    0 o \  ▓ ▓   ▓     ▓          ▓  ▓  ▓  ▓     ▓  ▓  ▓<br></br>
+                |       &gt;|  ▓▓    ▓▓▓▓  ▓    ▓▓▓▓  ▓  ▓  ▓▓▓▓     ▓  ▓▓▓▓<br></br>
+                |~~~~~~~~|  ▓ ▓      ▓  ▓          ▓  ▓  ▓  ▓     ▓    ▓ <br></br>
+                ----------  ▓  ▓  ▓▓▓▓  ▓▓▓▓       ▓▓▓▓  ▓  ▓  ▓▓▓▓   ▓▓ <br></br>
+              </p>
+              {/* <p>
+                ▓  ▓  ▓▓▓▓     ▓  ▓  ▓<br></br>
+                ▓  ▓  ▓  ▓     ▓  ▓  ▓<br></br>
+                ▓  ▓  ▓▓▓▓     ▓  ▓▓▓▓<br></br>
+                ▓  ▓  ▓  ▓     ▓    ▓ <br></br>
+                ▓▓▓▓  ▓  ▓  ▓▓▓▓   ▓▓ <br></br>
               </p> */}
             </h2>
             <h2 className="font-mono text-sm text-[#ECECEC] md:text-lg">
