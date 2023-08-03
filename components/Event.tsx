cdd7a31039b7b0f5046532dc0ac8bffb6ddc6619
Alexis Ngaing <ksankirara92@gmail.com>
2023-08-03 22:43:57 +0700

    change font size

--- components/Event.tsx
+++ components/Event.tsx
@@ -7,7 +7,7 @@ const Event: FC = () => {
   return (
     <>
       <div className="container px-6 py-16 md:px-24">
-        <h1 className="font-rubik text-3xl font-bold text-[#FAFFC4]">Event</h1>
+        <h1 className="font-rubik text-xl font-bold text-[#FAFFC4] md:text-3xl">Event</h1>
         <div className="my-10 w-80 rounded-xl bg-[#353520] p-6 shadow-2xl lg:h-3/6 lg:w-3/12">
           <a href="#">
             <Image alt="test" className="mx-auto h-auto rounded-xl" src={FotoTest4} />
