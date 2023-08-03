871fd94bb1e0a6e578dc235bf36ac356534cf3e2
Alexis Ngaing <ksankirara92@gmail.com>
2023-08-03 22:38:29 +0700

    fix responsive and change font size

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -8,14 +8,16 @@ const Gallery: FC = () => {
   return (
     <>
       <div className="container px-6 md:px-24">
-        <h1 className="pb-10 font-rubik text-3xl font-bold text-[#FAFFC4]">Galeri Kegiatan</h1>
-        <div className="w-80 rounded-xl bg-[#353520] md:flex md:w-full">
+        <h1 className="pb-10 font-rubik text-xl font-bold text-[#FAFFC4] md:text-3xl">
+          Galeri Kegiatan
+        </h1>
+        <div className="w-full rounded-xl bg-[#353520] md:flex">
           <div className="flex gap-6 p-6">
-            <Image alt="test" className="rounded-lg md:h-28 md:w-28" src={FotoTest2} />
-            <Image alt="test" className="rounded-lg md:h-28 md:w-28" src={FotoTest2} />
+            <Image alt="test" className="h-24 w-24 rounded-lg md:h-28 md:w-28" src={FotoTest2} />
+            <Image alt="test" className="h-24 w-24 rounded-lg md:h-28 md:w-28" src={FotoTest2} />
           </div>
           <div className="flex p-6 md:ml-auto md:w-5/12">
-            <div className="w-72 rounded-lg bg-[#4A4E24] shadow-2xl md:w-auto">
+            <div className="w-72 rounded-lg bg-[#4A4E24] px-4 shadow-2xl md:w-auto lg:px-0">
               <a href="#">
                 <Image
                   alt="test"
