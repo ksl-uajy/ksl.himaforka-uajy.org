704f7476d57157f24aa717df9ffc970ee70ddeca
stefanuspet <stefanuspetra.p@gmail.com>
2023-08-26 02:07:44 +0700

    fix firebaseConfig

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -17,15 +17,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-let app;
-let db;
-
-try {
-  app = initializeApp(firebaseConfig);
-  db = getFirestore(app);
-} catch (e) {
-  app = initializeApp.app;
-  db = getFirestore(app);
-}
-
-export { app, db };
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+export { db };
