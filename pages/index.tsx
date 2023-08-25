5c072441a79b026e91c50ab0a6a757f10e2fa28d
stefanuspet <stefanuspetra.p@gmail.com>
2023-08-25 10:22:41 +0700

    fix link Navbar - petra kun

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,10 @@ const Home = () => {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link href="/favicon.ico" rel="icon" />
         <link href="https://fonts.googleapis.com" rel="preconnect" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <div className="bg-gradient-to-r from-[#03151B] to-[#1D1903]">
         <Navbar />
